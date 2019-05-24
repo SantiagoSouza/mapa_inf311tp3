579abc3e16292623c1c2f3b981706c2a03770f19
SantiagoSouza <santiagogaudencio18@gmail.com>
2019-05-24 11:42:33 -0300

    2 commit

--- app/src/main/java/br/nom/souza/santiago/mapa_inf311tp3/Primeira.java
+++ app/src/main/java/br/nom/souza/santiago/mapa_inf311tp3/Primeira.java
@@ -2,6 +2,8 @@ package br.nom.souza.santiago.mapa_inf311tp3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Primeira extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Primeira extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_primeira);
+
+        ListView listview = (ListView) findViewById(R.id.menuPrincipal);
+        String[] dados = new String[] { "Minha casa na Cidade Natal", "Minha casa em Viçosa", "Meu departamento", "Fechar aplicativo" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dados);
+        listview.setAdapter(adapter);
     }
 }
